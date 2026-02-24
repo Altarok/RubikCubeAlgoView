209a32fe946e4aa7982ead7e5bbb67cfd9c5543f
Altarok <thom.hammerklinge@gmail.com>
2026-02-24 10:17:14 +0100

    WebStorm renaming is way too greedy, going to commit files one by one

--- src/model/geometry.ts
+++ src/model/geometry.ts
@@ -4,23 +4,32 @@ export class Coordinates {
    * @param x - pixels from left
    * @param y - pixels from top
    */
-  constructor(public readonly x: number, public readonly y: number) {}
-  toString(): string { return `${this.x},${this.y}`; }
+  constructor(public readonly x: number, public readonly y: number) {
+  }
+
+  toString = () => {
+    return `${this.x},${this.y}`;
+  }
 }
 
 /** Coordinates for -one- arrow. */
-export class ArrowCoordinates {
+export class ArrowCoords {
   /**
    * @param start - start coordinates of arrow
    * @param end - end coordinates of arrow
    */
-  constructor(public readonly start: Coordinates, public readonly end: Coordinates) {}
-  toString(): string { return `Arrow[from'${this.start}', to'${this.end}']`; }
+  constructor(public readonly start: Coordinates, public readonly end: Coordinates) {
+  }
+
+  toString = () => {
+    return `Arrow[from'${this.start}', to'${this.end}']`;
+  }
 }
 
 /** Coordinates for -multiple- arrows. */
-export type Arrows = ArrowCoordinates[];
+export type Arrows = ArrowCoords[];
 
+/** Simple dimensions */
 export interface Dimensions {
   readonly width: number;
   readonly height: number;
