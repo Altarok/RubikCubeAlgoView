7e0e77eb4f0c60213a1f756d5a09d18ec47421b0
Altarok <thom.hammerklinge@gmail.com>
2026-05-15 20:39:49 +0100

    configure new eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,45 +1,50 @@
 import tseslint from 'typescript-eslint';
 import obsidianmd from "eslint-plugin-obsidianmd";
 import globals from "globals";
-import { globalIgnores } from "eslint/config";
+import {globalIgnores} from "eslint/config";
 
 export default tseslint.config(
-	{
-		languageOptions: {
-			globals: {
-				...globals.browser,
-			},
-			parserOptions: {
-				projectService: {
-					allowDefaultProject: [
-						'eslint.config.js',
-						'manifest.json'
-					]
-				},
-				tsconfigRootDir: import.meta.dirname,
-				extraFileExtensions: ['.json']
-			},
-		},
-	},
-	...obsidianmd.configs.recommended,
   {
-    plugins: {
-      obsidianmd: obsidianmd,
-    },
-    rules: {
-      "obsidianmd/ui/sentence-case": [ "error", {
-        "acronyms": ["PLL", "OLL", "F2L", "CFOP"]
-      }]
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: [
+            'eslint.config.js',
+            'manifest.json'
+          ]
+        },
+        tsconfigRootDir: import.meta.dirname,
+        extraFileExtensions: ['.json']
+      },
     },
   },
-	globalIgnores([
-		"node_modules",
-		"esbuild.config.mjs",
-		"eslint.config.js",
-		"version-bump.mjs",
-		"versions.json",
-		"main.js",
-		"tests",
-		"coverage"
-	]),
+  ...obsidianmd.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parserOptions: {project: './tsconfig.json'},
+    },
+    rules: {
+      'obsidianmd/ui/sentence-case': [ 'warn',
+        {
+          brands: ['PLL', 'OLL', "Rubik's Cube"],
+          acronyms: ['OK'],
+          enforceCamelCaseLower: true,
+        },
+      ],
+    },
+  },
+  globalIgnores([
+    "node_modules",
+    "esbuild.config.mjs",
+    "eslint.config.js",
+    "version-bump.mjs",
+    "versions.json",
+    "main.js",
+    "tests",
+    "coverage"
+  ]),
 );
