4096f13e3eeee402a6a9665d9504be31f3f97260
Altarok <thom.hammerklinge@gmail.com>
2026-06-18 20:46:22 +0100

    make code block creator work

--- src/view/code-block-generator-modal.ts
+++ src/view/code-block-generator-modal.ts
@@ -1,6 +1,7 @@
 import RubikCubeAlgos from '../main'
 import {App, Modal} from 'obsidian'
 import {AnyInput, GenericModal, OutputData} from '@Altarok/obsidian-dev-utils/src'
+import {Settings} from "../settings/plugin-settings-tab";
 
 export class CodeBlockCreatorModal extends Modal {
   constructor(public readonly app: App, public readonly plugin: RubikCubeAlgos) {
@@ -14,7 +15,7 @@ export class CodeBlockCreatorModal extends Modal {
 
     /* Copy global settings */
     const globalSettings: Record<string, string> = {}
-    globalSettings['codeBlockId'] = '3'
+    globalSettings['codeBlockId'] = ''
     globalSettings['width'] = '3'
     globalSettings['height'] = '3'
     globalSettings['cubeColor'] = this.plugin.settings.cubeColor
@@ -25,9 +26,8 @@ export class CodeBlockCreatorModal extends Modal {
 
     const localSettings: Record<string, OutputData> = {}
 
-
-    const mandatoryInput = createMandatoryInput();
-    const optionalInput = createOptionalInput(globalSettings);
+    const mandatoryInput: Readonly<AnyInput>[] = createMandatoryInput();
+    const optionalInput: Readonly<AnyInput>[] = createOptionalInput(this.plugin.settings);
 
     new GenericModal(contentEl,
       {
@@ -35,8 +35,30 @@ export class CodeBlockCreatorModal extends Modal {
         mandatory: mandatoryInput,
         optional: optionalInput,
         output: localSettings,
-        createCodeBlock:():string => {
+        createCodeBlock: (): string => {
+          let code = ''
+          /* add main smiles notation */
+          if (localSettings.codeBlockId) code += `${localSettings.codeBlockId}\n`
 
+          /* add other options */
+          const settings: AnyInput[] = optionalInput.filter(
+            (setting): setting is AnyInput => !!setting && 'key' in setting)
+            .map(setting => setting)
+
+          for (const setting of settings) {
+            if (!setting) continue
+
+            const localValue = localSettings[setting.key]
+            if (localValue === undefined || localValue === '') continue
+
+            const key: string = setting.key
+            const globalValue = setting.current
+            if (globalValue === localValue) continue
+
+            code += `${key}: ${localValue}\n`
+          }
+
+          return `\`\`\`${code}\`\`\``
         },
         onUpdatePreview: (previewEl: HTMLElement): void => {
           previewEl.empty();
@@ -51,7 +73,7 @@ export class CodeBlockCreatorModal extends Modal {
   }
 }
 
-function createMandatoryInput(): Readonly<AnyInput>[]  {
+function createMandatoryInput(): Readonly<AnyInput>[] {
   return [
     {
       type: 'dropdown',
@@ -63,14 +85,32 @@ function createMandatoryInput(): Readonly<AnyInput>[]  {
   ]
 }
 
-function createOptionalInput(): Readonly<AnyInput>[]  {
+function createOptionalInput(pluginSettings: Settings): Readonly<AnyInput>[] {
   return [
-    // {
-    //   type: 'dropdown',
-    //   name: 'type of cube',
-    //   key: 'codeBlockId',
-    //   current: '',
-    //   dropdownOptions: ['rubikCubePLL', 'rubikCubeOLL'] as const
-    // }
+    {
+      type: 'dropdown',
+      name: 'width',
+      key: 'codeBlockId',
+      current: '3',
+      dropdownOptions: ['2', '3'] as const
+    },
+    {
+      type: 'dropdown',
+      name: 'height',
+      key: 'codeBlockId',
+      current: '3',
+      dropdownOptions: ['2', '3'] as const
+    },
+    {
+      type: 'color',
+      name: 'cube color',
+      key: 'cubeColor',
+      current: pluginSettings.cubeColor,
+    }, {
+      type: 'color',
+      name: 'arrow color',
+      key: 'arrowColor',
+      current: pluginSettings.arrowColor,
+    }
   ]
 }
