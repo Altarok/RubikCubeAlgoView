ac07bb004f8475b5b99c4963beb9aed55322b132
Altarok <thom.hammerklinge@gmail.com>
2026-08-02 21:28:37 +0100

    remove deprecated settings

--- src/settings/plugin-settings-tab.ts
+++ src/settings/plugin-settings-tab.ts
@@ -1,6 +1,5 @@
-import {App, PluginSettingTab, Setting, SettingDefinitionItem} from 'obsidian'
+import {App, PluginSettingTab, SettingDefinitionItem} from 'obsidian'
 import RubikCubeAlgos from '../main'
-import {RegEx} from '../parser/regex-util'
 import {Strings} from 'consts/strings'
 
 export type CubeColors = {
@@ -24,15 +23,6 @@ export const DefaultSettings: Settings = {
   activateCommandCodeblockExamples: true,
   activateCommandCodeblockTemplates: true,
   activateRibbonIconForCodeblockCreator: true
-}
-
-function addHashPrefixIfMissing(color: string) {
-  if (!color.startsWith('#')) color = `#${color}`
-  return color
-}
-
-function isValidColorInput(color: string): boolean {
-  return RegEx.isColorHexValueWithOptionalPrefix(color)
 }
 
 export class RubikCubeAlgoSettingsTab extends PluginSettingTab {
@@ -108,101 +98,4 @@ export class RubikCubeAlgoSettingsTab extends PluginSettingTab {
     ] // end group: colors
   }
 
-  display(): void {
-
-    const {containerEl} = this
-
-    containerEl.empty()
-
-    this.addQuickStartGuide(containerEl)
-    this.addHorizontalSeparator(containerEl)
-    // this.addColorSettingsHeader(containerEl)
-    this.addColorSettingsCube(new Setting(containerEl))
-    this.addColorSettingsArrows(new Setting(containerEl))
-    this.addColorSettingsReset(new Setting(containerEl))
-  }
-
-  private addColorSettingsReset(setting: Setting) {
-    setting.setName('Reset colors').setDesc('Restore default color values.')
-      .addButton((cb) => cb
-        .setButtonText('Reset')
-        .setDestructive()
-        .onClick(async () => {
-          this.tempColorInput.cubeColor = DefaultSettings.cubeColor
-          this.tempColorInput.arrowColor = DefaultSettings.arrowColor
-
-          this.plugin.settings.cubeColor = DefaultSettings.cubeColor
-          this.plugin.settings.arrowColor = DefaultSettings.arrowColor
-          await this.plugin.saveSettings()
-          this.display()
-        })
-      )
-  }
-
-  private addColorSettingsArrows(setting: Setting) {
-    setting.setName('Arrow color').setDesc('Default color for algorithm arrows. Resets to #0088ff (sky blue).')
-      .addText((text) => text.setValue(this.tempColorInput.arrowColor).setDisabled(true))
-      .addColorPicker(color => color.setValue(this.tempColorInput.arrowColor).onChange((value) => this.changeCurrentArrowColor(value)))
-      .addExtraButton(button => button.setTooltip('Save').setIcon('save').onClick(async () => {
-          let isValid: boolean = isValidColorInput(this.tempColorInput.arrowColor)
-          let valueToSafe = isValid ? addHashPrefixIfMissing(this.tempColorInput.arrowColor) : DefaultSettings.arrowColor
-          this.tempColorInput.arrowColor = valueToSafe
-          this.plugin.settings.arrowColor = valueToSafe
-          await this.plugin.saveSettings()
-          this.display()
-        }
-      ))
-  }
-
-  private addColorSettingsCube(setting: Setting) {
-    setting.setName('Cube color').setDesc('Default color for cube faces. Resets to #ffff00 (yellow).')
-      .addText((text) => text.setValue(this.tempColorInput.cubeColor).setDisabled(true))
-      .addColorPicker(color => color.setValue(this.tempColorInput.cubeColor).onChange((value) => this.changeCurrentCubeColor(value)))
-      .addExtraButton(button => button.setTooltip('Save').setIcon('save').onClick(async () => {
-          let isValid: boolean = isValidColorInput(this.tempColorInput.cubeColor)
-          let valueToSafe = isValid ? addHashPrefixIfMissing(this.tempColorInput.cubeColor) : DefaultSettings.cubeColor
-          this.tempColorInput.cubeColor = valueToSafe
-          this.plugin.settings.cubeColor = valueToSafe
-          await this.plugin.saveSettings()
-          this.display()
-        }
-      ))
-  }
-
-  private changeCurrentCubeColor(hexColor: string) {
-    this.tempColorInput.cubeColor = hexColor
-    if (isValidColorInput(hexColor)) {
-      hexColor = addHashPrefixIfMissing(hexColor)
-      if (this.plugin.settings.cubeColor !== hexColor) {
-        this.plugin.settings.cubeColor = hexColor
-        this.plugin.rerenderCodeblocks()
-        this.display()
-      }
-    }
-  }
-
-  private changeCurrentArrowColor(hexColor: string) {
-    this.tempColorInput.arrowColor = hexColor
-    if (isValidColorInput(hexColor)) {
-      hexColor = addHashPrefixIfMissing(hexColor)
-      if (this.plugin.settings.arrowColor !== hexColor) {
-        this.plugin.settings.arrowColor = hexColor
-        this.plugin.rerenderCodeblocks()
-        this.display()
-      }
-    }
-  }
-
-  private addHorizontalSeparator(containerEl: HTMLElement) {
-    containerEl.createEl('hr')
-  }
-
-  private addQuickStartGuide(containerEl: HTMLElement) {
-    new Setting(containerEl).setName('Commands').setDesc('Use "Cmd/Ctrl + P" and search "Rubik" to use built-in commands.')
-    new Setting(containerEl).setName('Quick start guide').setDesc('Use command to add quick start guide to any note. (~10k characters)')
-    new Setting(containerEl).setName('Just want finished code blocks?').setDesc('Use commands to insert complete OLL or PLL algorithm lists.')
-    new Setting(containerEl).setName('Searching for templates?').setDesc('Use commands to insert code block templates with explanatory comments.')
-    // new Setting(containerEl).setName('Notation').setDesc('Algorithm notation updates automatically when rotating.')
-    // new Setting(containerEl).setName('Customization').setDesc('Set width, height (2-10), a'nd hex colors per block.')
-  }
 }
